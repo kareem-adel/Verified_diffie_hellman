165bdce52a122cefdacd3b1540442b7d29180189
kareem-adel <kareem.adel.fathi@gmail.com>
2017-03-29 22:41:03 +0200

    Send key done

--- src/mainApp.java
+++ src/mainApp.java
@@ -1,8 +1,22 @@
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SignatureException;
+
 /**
  * Created by kareem on 3/29/2017.
  */
 public class mainApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        user sender = new user(7, 7);
+        try {
+            sender.sendKey();
+        } catch (SignatureException | NoSuchProviderException | NoSuchAlgorithmException | IOException | InvalidKeyException e) {
+            e.printStackTrace();
+        }
 
+        user receiver = new user(7, 7);
+        System.out.println(sender.receiveKey());
     }
 }
--- src/user.java
+++ src/user.java
@@ -1,3 +1,6 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.security.*;
 import java.util.Random;
 
 /**
@@ -17,19 +20,55 @@ public class user {
         y = (int) (Math.pow(a, x) % q);
     }
 
-    public void calculateSharedKey(int remoteY){
+    public void calculateSharedKey(int remoteY) {
         k = (int) (Math.pow(remoteY, x) % q);
     }
 
 
     //kareem
-    public void sendKey(){
+    public void sendKey() throws SignatureException, NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException, IOException {
+        String s = String.valueOf(y);
+        BufferedWriter writer;
+        writer = Files.newBufferedWriter(new File("y").toPath());
+        writer.write(s, 0, s.length());
+        writer.flush();
+        writer.close();
 
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
+        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+        keyGen.initialize(1024, random);
+        KeyPair pair = keyGen.generateKeyPair();
+        PrivateKey priv = pair.getPrivate();
+        PublicKey pub = pair.getPublic();
+
+        Signature dsa = Signature.getInstance("SHA1withDSA", "SUN");
+        dsa.initSign(priv);
+
+        FileInputStream fis = new FileInputStream("y");
+        BufferedInputStream bufin = new BufferedInputStream(fis);
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = bufin.read(buffer)) >= 0) {
+            dsa.update(buffer, 0, len);
+        }
+        bufin.close();
+
+        byte[] realSig = dsa.sign();
+
+        FileOutputStream sigfos = new FileOutputStream("sy");
+        sigfos.write(realSig);
+        sigfos.close();
+
+        byte[] key = pub.getEncoded();
+        FileOutputStream keyfos = new FileOutputStream("puk");
+        keyfos.write(key);
+        keyfos.close();
     }
 
 
-    //omar
-    public void receiveKey(){
+    //omar (should return the shared key)
+    public int receiveKey() {
 
+        return 0;
     }
 }
