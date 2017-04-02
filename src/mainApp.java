a6a112701423365546eba709028b04fc15c38618
Omar Adel <omaradel9545@gmail.com>
2017-04-02 23:40:43 +0200

    [Add] receiver added

--- src/mainApp.java
+++ src/mainApp.java
@@ -1,22 +1,32 @@
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.SignatureException;
+import java.math.BigInteger;
 
-/**
- * Created by kareem on 3/29/2017.
- */
-public class mainApp {
-    public static void main(String[] args) {
-        user sender = new user(7, 7);
-        try {
-            sender.sendKey();
-        } catch (SignatureException | NoSuchProviderException | NoSuchAlgorithmException | IOException | InvalidKeyException e) {
-            e.printStackTrace();
-        }
+public class mainApp
+{
+	public static void main(String[] args)
+	{
+		int q = 353;
+		int a = 3;
 
-        user receiver = new user(7, 7);
-        System.out.println(sender.receiveKey());
-    }
+		user sender = new user(q, a, "user1");
+		user receiver = new user(q, a, "user2");
+		try
+		{
+			sender.sendKey();
+			receiver.sendKey();
+			BigInteger sharedKey1 = sender.receiveKey(receiver.name);
+			BigInteger sharedKey2 = receiver.receiveKey(sender.name);
+			if (sharedKey1.equals(new BigInteger("-1")) || sharedKey2.equals(new BigInteger("-1")))
+			{
+				System.out.println("Valued to verify signature");
+			} else
+			{
+				System.out.println("Sharedkey 1: " + sharedKey1);
+				System.out.println("Sharedkey 2: " + sharedKey2);
+				System.out.println("Shared keys are equal? : " + (sharedKey1.equals(sharedKey2)));
+			}
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
--- src/user.java
+++ src/user.java
@@ -1,74 +1,138 @@
-import java.io.*;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
-import java.security.*;
-import java.util.Random;
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.SecureRandom;
+import java.security.Signature;
+import java.security.SignatureException;
+import java.security.spec.X509EncodedKeySpec;
 
-/**
- * Created by kareem on 3/29/2017.
- */
-public class user {
+public class user
+{
+	BigInteger q;
+	int x;// Private key
+	BigInteger y;// Public key
+	BigInteger k;// Shared key
+	String name;
 
-    int q;
-    //int a;
-    int x;//private
-    int y;//public
-    int k;//sharedkey
+	public user(int q, int a, String name)
+	{
+		this.q = new BigInteger(q + "");
+		this.x = (int) (Math.random() * (q - 2) + 1);
+		BigInteger aBig = new BigInteger(a + "");
+		BigInteger temp = aBig.pow(x);
+		y = temp.mod(this.q);
+		this.name = name;
+	}
 
-    public user(int q, int a) {
-        this.q = q;
-        x = (int) (Math.random() % q);
-        y = (int) (Math.pow(a, x) % q);
-    }
+	public BigInteger calculateSharedKey(BigInteger remoteY)
+	{
+		BigInteger temp = remoteY.pow(x);
+		k = temp.mod(q);
+		return k;
+	}
 
-    public void calculateSharedKey(int remoteY) {
-        k = (int) (Math.pow(remoteY, x) % q);
-    }
+	// Kareem
+	public void sendKey() throws SignatureException, NoSuchProviderException, NoSuchAlgorithmException,
+			InvalidKeyException, IOException
+	{
+		String s = y + "";
+		BufferedWriter writer;
+		writer = Files.newBufferedWriter(new File(this.name + "y").toPath());
+		writer.write(s, 0, s.length());
+		writer.flush();
+		writer.close();
 
+		KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
+		SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
+		keyGen.initialize(1024, random);
+		KeyPair pair = keyGen.generateKeyPair();
+		PrivateKey priv = pair.getPrivate();
+		PublicKey pub = pair.getPublic();
 
-    //kareem
-    public void sendKey() throws SignatureException, NoSuchProviderException, NoSuchAlgorithmException, InvalidKeyException, IOException {
-        String s = String.valueOf(y);
-        BufferedWriter writer;
-        writer = Files.newBufferedWriter(new File("y").toPath());
-        writer.write(s, 0, s.length());
-        writer.flush();
-        writer.close();
+		Signature dsa = Signature.getInstance("SHA1withDSA", "SUN");
+		dsa.initSign(priv);
 
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
-        SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
-        keyGen.initialize(1024, random);
-        KeyPair pair = keyGen.generateKeyPair();
-        PrivateKey priv = pair.getPrivate();
-        PublicKey pub = pair.getPublic();
+		FileInputStream fis = new FileInputStream(this.name + "y");
+		BufferedInputStream bufin = new BufferedInputStream(fis);
+		byte[] buffer = new byte[1024];
+		int len;
+		while ((len = bufin.read(buffer)) >= 0)
+		{
+			dsa.update(buffer, 0, len);
+		}
+		bufin.close();
 
-        Signature dsa = Signature.getInstance("SHA1withDSA", "SUN");
-        dsa.initSign(priv);
+		byte[] realSig = dsa.sign();
 
-        FileInputStream fis = new FileInputStream("y");
-        BufferedInputStream bufin = new BufferedInputStream(fis);
-        byte[] buffer = new byte[1024];
-        int len;
-        while ((len = bufin.read(buffer)) >= 0) {
-            dsa.update(buffer, 0, len);
-        }
-        bufin.close();
+		FileOutputStream sigfos = new FileOutputStream(this.name + "sy");
+		sigfos.write(realSig);
+		sigfos.close();
 
-        byte[] realSig = dsa.sign();
+		byte[] key = pub.getEncoded();
+		FileOutputStream keyfos = new FileOutputStream(this.name + "puk");
+		keyfos.write(key);
+		keyfos.close();
+	}
 
-        FileOutputStream sigfos = new FileOutputStream("sy");
-        sigfos.write(realSig);
-        sigfos.close();
+	// Omar
+	public BigInteger receiveKey(String userName) throws Exception
+	{
+		FileInputStream keyfis = new FileInputStream(userName + "puk");
+		byte[] encKey = new byte[keyfis.available()];
+		keyfis.read(encKey);
+		keyfis.close();
 
-        byte[] key = pub.getEncoded();
-        FileOutputStream keyfos = new FileOutputStream("puk");
-        keyfos.write(key);
-        keyfos.close();
-    }
+		X509EncodedKeySpec pubKeySpec = new X509EncodedKeySpec(encKey);
+		KeyFactory keyFactory = KeyFactory.getInstance("DSA", "SUN");
+		PublicKey pubKey = keyFactory.generatePublic(pubKeySpec);
 
+		FileInputStream sigfis = new FileInputStream(userName + "sy");
+		byte[] sigToVerify = new byte[sigfis.available()];
+		sigfis.read(sigToVerify);
+		sigfis.close();
 
-    //omar (should return the shared key)
-    public int receiveKey() {
+		Signature sig = Signature.getInstance("SHA1withDSA", "SUN");
+		sig.initVerify(pubKey);
 
-        return 0;
-    }
+		FileInputStream datafis = new FileInputStream(userName + "y");
+		BufferedInputStream bufin = new BufferedInputStream(datafis);
+
+		byte[] buffer = new byte[1024];
+		int len;
+		while (bufin.available() != 0)
+		{
+			len = bufin.read(buffer);
+			sig.update(buffer, 0, len);
+		}
+		bufin.close();
+
+		boolean verifies = sig.verify(sigToVerify);
+		System.out.println(userName + " signature verifies: " + verifies);
+
+		if (!verifies)
+		{
+			return new BigInteger("-1");
+		}
+
+		BufferedReader reader = new BufferedReader(new FileReader(userName + "y"));
+		BigInteger line = new BigInteger(reader.readLine());
+		reader.close();
+
+		return calculateSharedKey(line);
+	}
 }
